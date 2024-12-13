5180498f5a9c4c1a4d6f6853afbc8a11eec21d68
dev4ushakiran <dev4ushakiran@gmail.com>
2024-12-13 20:40:45 +0530

    page-5-page-6-functionalities

--- form2/form2.js
+++ form2/form2.js
@@ -85,28 +85,28 @@ function updateProgress2(formId) {
 }
 
 function handleMutualExclusiveCheckboxes2(formId) {
-    const checkboxes = document.querySelectorAll(`#${formId} input[type="checkbox"], #${formId} input[type="radio"]`);
+    const radios = document.querySelectorAll(`#${formId} input[type="radio"]`);
 
-    const checkboxGroups = {};
+    const radioGroups = {};
 
-    checkboxes.forEach(checkbox => {
-        const groupName = checkbox.name;
+    radios.forEach(radio => {
+        const groupName = radio.name;
 
-        if (!checkboxGroups[groupName]) {
-            checkboxGroups[groupName] = [];
+        if (!radioGroups[groupName]) {
+            radioGroups[groupName] = [];
         }
 
-        checkboxGroups[groupName].push(checkbox);
+        radioGroups[groupName].push(radio);
 
-        checkbox.addEventListener('change', () => {
-            checkboxGroups[groupName].forEach(otherCheckbox => {
-                if (otherCheckbox !== checkbox && (otherCheckbox.type === checkbox.type)) {
+        radio.addEventListener('change', () => {
+            radioGroups[groupName].forEach(otherCheckbox => {
+                if (otherCheckbox !== radio && (otherCheckbox.type === radio.type)) {
                     otherCheckbox.checked = false;
                 }
             });
 
             updateProgress2(formId);
-            // toggleHighBpFields(formId); 
+      
             toggleFields2(formId, toggleFieldsConfig2);
         });
     });
@@ -192,7 +192,7 @@ function toggleFields2(formId, config) {
                 .filter(element => element.id)
                 .map(element => element.id);
 
-                console.log("Printing Child Ids",childIds)
+              
 
             if (radiobut && radiobut.checked) {
                 fieldElement.style.display = 'block';
@@ -221,20 +221,32 @@ const toggleFieldsConfig2 = [
         radioId: "addressYes1",
         fieldsToShow: ["priorAdd"],
     },
+    // {
+    //     radioId:"voilationYes",
+    //     fieldsToShow:["voilationDetail"]
+    // },
+    // {
+    //     radioId:"voilationYes1",
+    //     fieldsToShow:["voilationDetail1"]
+    // },
+    // {
+    //     radioId:"accidentYes",
+    //     fieldsToShow:["accident"]
+    // },
+    // { radioId:"accidentYes1",
+    //   fieldsToShow:["accident1"],
+    // },
     {
-        radioId:"voilationYes",
-        fieldsToShow:["voilationDetail"]
+        radioId:"licenseDenyYes",
+        fieldsToShow:["denyExplanation"],
     },
     {
-        radioId:"voilationYes1",
-        fieldsToShow:["voilationDetail1"]
+        radioId:"licenseSuspendedYes",
+        fieldsToShow:["suspendedExplanation"],
     },
     {
-        radioId:"accidentYes",
-        fieldsToShow:["accident"]
-    },
-    { radioId:"accidentYes1",
-      fieldsToShow:["accident1"],
+      radioId:"militaryYes",
+      fieldsToShow:["fmcsa"]
     }
 
 ]
@@ -466,6 +478,179 @@ function getValidationRule(input) {
     return rules[validationType] || null;
 }
 
+
+
+let violationCount = 0;
+
+function handleRadioChange(event) {
+    const container = document.getElementById("violation-container");
+
+    if (event.target.value === "yes") {
+        // Increment the violation count
+        violationCount++;
+
+        // Create a unique ID for the new violation detail
+        const violationId = `violationDetail-${violationCount}`;
+
+        // Create the template for the new violation detail
+        const template = `
+            <div id="${violationId}" class="violation-detail" data-index="${violationCount}">
+                <div class="address-container mt-3">
+                    <div class="col-12">
+                        <label class="question-label">Date of Conviction</label>
+                        <input type="date" class="dab" name="applicantdob-${violationCount}" id="voilationDate-${violationCount}" onchange="updateProgress2('form2')">
+                    </div>
+                    <div class="col-12 mt-3">
+                        <label class="question-label">Offence</label>
+                        <input type="text" data-validate="text" name="offence-${violationCount}" autocomplete="off" class="form-control" id="voilationOffence-${violationCount}" onchange="updateProgress2('form2')">
+                    </div>
+                    <div class="col-12 mt-3 mb-3">
+                        <label class="question-label">Location</label>
+                        <input type="text" data-validate="text" name="voilationLocation-${violationCount}" autocomplete="off" class="form-control" id="voilationLocation-${violationCount}" onchange="updateProgress2('form2')">
+                    </div>
+                    <div class="mt-3 mb-2">
+                        <label class="question-label">Type Of Vehicle Operated</label>
+                        <div class="form-check col-12 mt-2">
+                            <input class="form-check-input box" type="radio" id="offenceCommercial-${violationCount}" name="offenceVehicle-${violationCount}" value="Commercial" onchange="updateProgress2('form2')">
+                            <label for="offenceCommercial-${violationCount}" class="label ms-3">Commercial</label>
+                        </div>
+                        <div class="form-check col-12 mt-2">
+                            <input class="form-check-input box" type="radio" id="offencePrivate-${violationCount}" name="offenceVehicle-${violationCount}" value="Private" onchange="updateProgress2('form2')">
+                            <label for="offencePrivate-${violationCount}" class="label ms-3">Private</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 d-flex flex-column mt-2">
+                    <label>Do you have additional traffic violations to add?</label>
+                    <div class="form-check d-flex align-items-end">
+                        <input class="form-check-input" type="radio" value="no" id="violationNo-${violationCount}" name="trafficVoilation-${violationCount}" onchange="handleRadioChange(event)">
+                        <label class="form-check-label label ms-4" for="violationNo-${violationCount}">No</label>
+                    </div>
+                    <div class="form-check mt-2">
+                        <input class="form-check-input" type="radio" value="yes" id="violationYes-${violationCount}" name="trafficVoilation-${violationCount}" onchange="handleRadioChange(event)">
+                        <label class="form-check-label ms-4" for="violationYes-${violationCount}">Yes</label>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        // Append the new section
+        container.insertAdjacentHTML("beforeend", template);
+    } else if (event.target.value === "no") {
+        const currentViolation = event.target.closest(".violation-detail");
+
+        if (!currentViolation) {
+            // This is the first set of "Yes/No" buttons outside the .violation-detail
+            while (container.firstChild) {
+                container.removeChild(container.firstChild);
+            }
+            violationCount = 0;
+            return;
+        }
+
+        const currentViolationIndex = parseInt(currentViolation.dataset.index);
+
+        // Remove all subsequent violations
+        const allViolations = Array.from(container.children);
+        allViolations.forEach(violation => {
+            const violationIndex = parseInt(violation.dataset.index);
+            if (violationIndex > currentViolationIndex) {
+                container.removeChild(violation);
+            }
+        });
+
+        // Update the violationCount to match the remaining violations
+        violationCount = currentViolationIndex;
+    }
+}
+
+
+let accidentCount = 0;
+
+function handleAccidentChange(event) {
+    const container = document.getElementById("accident-container");
+
+    if (event.target.value === "yes") {
+        // Increment the accident count
+        accidentCount++;
+
+        // Create a unique ID for the new accident detail
+        const accidentId = `accidentDetail-${accidentCount}`;
+
+        // Create the template for the new accident detail
+        const template = `
+            <div id="${accidentId}" class="accident-detail" data-index="${accidentCount}">
+                <div class="address-container mb-2 mt-3">
+                    <div class="col-12">
+                        <label class="question-label">Date of Accident</label>
+                        <input type="date" class="dab" name="accidentDate-${accidentCount}" id="accidentDate-${accidentCount}" oninput="handleDateChange2(event)" placeholder="MM/DD/YYYY" onchange="updateProgress2('form2')">
+                    </div>
+
+                    <div class="col-12 d-flex flex-column mt-2">
+                        <div>
+                            <label class="question-label">Fatalities or Personal Injuries</label>
+                        </div>
+                        <div class="form-check mt-2">
+                            <input class="form-check-input" type="radio" value="no" id="fatalities-${accidentCount}" name="fatalities-${accidentCount}" onchange="updateProgress2('form2')">
+                            <label class="form-check-label label ms-4" for="fatalities-${accidentCount}">Fatalities</label>
+                        </div>
+                        <div class="form-check mt-2">
+                            <input class="form-check-input" type="radio" value="yes" id="injuries-${accidentCount}" name="fatalities-${accidentCount}" onchange="updateProgress2('form2')">
+                            <label class="form-check-label ms-4" for="injuries-${accidentCount}">Personal Injuries</label>
+                        </div>
+                        <div class="col-12 mt-2">
+                            <label class="question-label">Circumstances of Accident</label>
+                            <textarea name="circumstances-${accidentCount}" id="accidentExplanation-${accidentCount}" rows="4" class="form-control mt-2 txtfeild"></textarea>
+                        </div>
+                        <div class="mb-3 mt-3">
+                            <label class="question-label">Attachment (Optional)</label>
+                            <input type="file" id="fileInput-${accidentCount}" class="form-control mt-2" />
+                        </div>
+                    </div>
+                </div>
+                    <div class="col-12 d-flex flex-column mt-2">
+                        <label class="question-label">Do you have additional accidents to add?</label>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" value="no" id="accidentNo-${accidentCount}" name="additionalAccident-${accidentCount}" onchange="handleAccidentChange(event)">
+                            <label class="form-check-label label ms-4" for="accidentNo-${accidentCount}">No</label>
+                        </div>
+                        <div class="form-check mt-2">
+                            <input class="form-check-input" type="radio" value="yes" id="accidentYes-${accidentCount}" name="additionalAccident-${accidentCount}" onchange="handleAccidentChange(event)">
+                            <label class="form-check-label ms-4" for="accidentYes-${accidentCount}">Yes</label>
+                        </div>
+                    </div>
+            </div>
+        `;
+
+        // Append the new accident detail template
+        container.insertAdjacentHTML("beforeend", template);
+    } else if (event.target.value === "no") {
+        const currentAccident = event.target.closest(".accident-detail");
+
+        if (!currentAccident) {
+            // This is the first set of "Yes/No" buttons outside the .accident-detail
+            while (container.firstChild) {
+                container.removeChild(container.firstChild);
+            }
+            accidentCount = 0;
+            return;
+        }
+
+        const currentAccidentIndex = parseInt(currentAccident.dataset.index);
+
+        // Remove all subsequent accidents
+        const allAccidents = Array.from(container.children);
+        allAccidents.forEach(accident => {
+            const accidentIndex = parseInt(accident.dataset.index);
+            if (accidentIndex > currentAccidentIndex) {
+                container.removeChild(accident);
+            }
+        });
+
+        // Update the accidentCount to match the remaining accidents
+        accidentCount = currentAccidentIndex;
+    }
+}
 
 
 initializeForm2('form2');
